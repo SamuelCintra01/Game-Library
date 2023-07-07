3208b425afab19d79e24c1043b603cc0afce5061
Samuel Cintra <samuelcintramartins01@gmail.com>
2023-07-07 16:58:31 -0300

    Added functionality to prevent page refresh when clicking buttons within the form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 export function App() {
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
   return (
     <div id="app">
       <h1>Biblioteca de jogos</h1>
@@ -11,7 +15,9 @@ export function App() {
           <label htmlFor="cover">Capa:</label>
           <input type="text" name="cover" id="cover" />
         </div>
-        <button type="submit">Adicionar à biblioteca</button>
+        <button type="submit" onClick={handleSubmit}>
+          Adicionar à biblioteca
+        </button>
       </form>
     </div>
   );
