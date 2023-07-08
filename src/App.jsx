7256829fd2b76139e8ba1e28d0f134a5c2117050
Samuel Cintra <samuelcintramartins01@gmail.com>
2023-07-08 15:06:07 -0300

    Added functions and hooks for game addition to the game library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 export function App() {
+  const [games, setGames] = useState([]);
+  const [title, setTitle] = useState();
+  const [cover, setCover] = useState();
+
+  const addGame = ({ title, cover }) => {
+    const id = Math.floor(Math.random() * 1000000);
+    const game = { id, title, cover };
+    setGames((state) => [...state, game]);
+  };
+
   const handleSubmit = (ev) => {
     ev.preventDefault();
+    addGame({ title, cover });
+    setTitle("");
+    setCover("");
   };
 
   return (
@@ -9,14 +24,37 @@ export function App() {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="title">Título:</label>
-          <input type="text" name="title" id="title" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="cover">Capa:</label>
-          <input type="text" name="cover" id="cover" />
+          <input
+            type="text"
+            name="cover"
+            id="cover"
+            value={cover}
+            onChange={(e) => setCover(e.target.value)}
+          />
         </div>
         <button type="submit">Adicionar à biblioteca</button>
       </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.cover} alt={game.cover} />
+            <div>
+              <h2>{game.title}</h2>
+              <button>Remover</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
